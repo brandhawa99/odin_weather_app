69ba32196c3832b1bcfa3d39b1bc76afd1a0e86b
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-01-12 14:26:15 -0800

    final prodcut

--- app.js
+++ app.js
@@ -1,26 +1,73 @@
-
-
-
 const api_key = '988aac9b7fd3931f769476f1cfa470d1'
-let city = 'vancouver'
+
+
+const submit_button = document.querySelector('.search')
+const input = document.querySelector('.input')
+const get_unit = document.querySelector('.cel')
 let unit = 'metric'
+let city = 'vancouver'
+
+submit_button.addEventListener('click',() => {
+    let city = input.value;
+    getData(city,unit);
+})
+input.addEventListener('keyup',(e) =>{
+    if(e.keyCode ===13){
+        let city = input.value;
+        getData(city,unit);
+
+    }
+})
+
+
+get_unit.addEventListener('click',() =>{
+    let city = document.querySelector('#city').textContent
+    const CorF = document.querySelectorAll('.CorF')
+    if(unit == 'metric'){
+        unit = 'imperial'
+        CorF.forEach(ele =>{
+            ele.textContent = "F"
+        })
+
+        getData(city,unit)
+    }
+    else{
+        unit = 'metric'
+        getData(city,unit)
+                CorF.forEach(ele =>{
+            ele.textContent = "C"
+        })
+    }
+
+})
 
 
 
-async function getData(){
+async function getData(city,unit){
     try {
         const response  = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${unit}&appid=988aac9b7fd3931f769476f1cfa470d1`,{mode:'cors'}); 
         const data  = await response.json(); 
+        // let iconID = data.weather[0].icon;
 
         document.querySelector('#city').textContent = data.name
         document.querySelector('#country').textContent = data.sys.country
         document.querySelector('.current').textContent = data.main.temp
         document.querySelector('.feels-like').textContent = data.main.feels_like
         document.querySelector('.desc').textContent = data.weather[0].description
-        console.log(data);
+        let img = document.querySelector('img')
+        img.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
         
     } catch (error) {
-        console.log('not a city')
+        // input.value = ('not a city')
+        let val = input.value
+
+        document.querySelector('#city').textContent = `${val} is not a city `
+        document.querySelector('#country').textContent = ""
+        document.querySelector('.current').textContent = ""
+        document.querySelector('.feels-like').textContent =""
+        document.querySelector('.desc').textContent = ""
+        let img = document.querySelector('img')
+        img.src = ""; 
         
     }
 
@@ -28,4 +75,7 @@ async function getData(){
 
 
 
-getData();
+
+
+
+getData(city,unit);
